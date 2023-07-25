5364918fc397ffb0167e0508f95810b19b28ad70
michaelgogue <134240873+michaelgogue@users.noreply.github.com>
2023-07-24 21:30:59 -0700

    Add files via upload
    Using Modules

---
+++ mathtools.psm1
@@ -0,0 +1,25 @@
+﻿#Mathtools.psm1
+
+function Get-addition {
+  Param (
+    $Num1,
+    $Num2
+  )
+  return $num1 + $num2
+}
+
+function Get-Multiplication {
+  Param (
+    $Num1,
+    $Num2
+  )
+  return $num1 * $num2
+}
+
+
+# TO IMPORT RUN THE FOLLOWING SCRIPT
+# import-module C:\scripts\mathtools.psm1
+# get-addition -num1 321 -num2 54
+# get-multiplication -num1 321 -num2 54
+
+#if module was updated you need to remove the module using remove-module NAME and then import again
